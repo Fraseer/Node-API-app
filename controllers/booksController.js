91b297933525978b3bd9f7a9fb452e08616b5ca5
Fraseer <Fraser.Hughes@btinternet.com>
2021-11-04 13:13:32 +0100

    Sets up index action for books

---
+++ controllers/booksController.js
@@ -0,0 +1,10 @@
+const models = require("../models");
+
+const booksController = {
+  async index(request, response, next) {
+    const bookIndex = await models.Book.findAll();
+    response.send({ books: bookIndex });
+  },
+};
+
+module.exports = booksController;
